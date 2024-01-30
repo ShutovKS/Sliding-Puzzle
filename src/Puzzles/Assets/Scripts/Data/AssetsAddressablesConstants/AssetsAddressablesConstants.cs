0e9f9b0cd6e8f7886ef347f535d6c622aa480919
ShutovKS <schutow.kir@yandex.ru>
2024-01-31 02:19:56 +0300

    Remove old addresslables paths

--- src/Puzzles/Assets/Scripts/Data/AssetsAddressablesConstants/AssetsAddressablesConstants.cs
+++ src/Puzzles/Assets/Scripts/Data/AssetsAddressablesConstants/AssetsAddressablesConstants.cs
@@ -6,7 +6,5 @@
         public const string MAIN_MENU_SCREEN = "Screens/MainMenu";
         public const string IN_GAME_MENU_SCREEN = "Screens/InGameMenu";
         public const string FOLDING_THE_PUZZLE_SCREEN = "Screens/FoldingThePuzzle";
-        public const string PUZZLE_INFORMATION_SCREEN = "Screens/PuzzleInformation";
-        public const string CATEGORY_INFORMATION_SCREEN = "Screens/CategoryInformation";
     }
 }
